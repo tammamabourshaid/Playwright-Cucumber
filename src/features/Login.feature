d54f4104c5e9547d7c5bfe0a67bb145e0cf06029
Tammam Abou-Rshaid <tammam.abourshaid@gmail.com>
2026-06-05 23:00:22 +0200

    update

--- src/features/Login.feature
+++ src/features/Login.feature
@@ -1,11 +1,15 @@
 Feature: Webdriveruniversity - Login Page
 
+    #Background: Pre conditions
+    #    Given I navigate to the webdriveruniversity homepage
+    #    When I click on the login portal button
+    #    And I switch to the new browser tab
+
     Scenario Outline: Validate valid & invalid login
-        Given I navigate to the webdriveruniversity homepage
-        When I click on the login portal button
-        And I switch to the new browser tab
+        Given I navigate to the webdriveruniversity login page
         And I type a username <username>
         And I type a password <password>
+        #And I wait for 2 seconds
         And I click on the login button
         Then I should be presented with an alert box which contains text '<expectedAlertText>'
 
--- src/step-definitions/Login_Steps.ts
+++ src/step-definitions/Login_Steps.ts
@@ -1,5 +1,8 @@
 import { When, Then } from "@cucumber/cucumber";
-import { pageFixture } from "./browserContextFixture";
+import { pageFixture } from "./hooks/browserContextFixture";
+import {expect} from "@playwright/test";
+
+let alertText: string;
 
 When('I type a username {word}', async (username: string) => {
     await pageFixture.page.getByPlaceholder("Username").fill(username);
@@ -10,12 +13,19 @@ When('I type a password {word}', async (password: string) => {
 });
 
 When('I click on the login button', async () => {
+    await pageFixture.page.on("dialog", async (alert) => {
+        alertText = alert.message();
+        //console.log(alertText);
+        await alert.accept();
+    })
+
     const loginButton = await pageFixture.page.locator("#login-button");
     await loginButton.hover();
     await loginButton.click({ force: true });
+    //await pageFixture.page.waitForTimeout(2000);
 });
 
 
-Then('I should be presented with an alert box which contains text {string}', async (string) => {
-
+Then('I should be presented with an alert box which contains text {string}', async (expectedAlertText: string) => {
+    expect(alertText).toBe(expectedAlertText);
 });
